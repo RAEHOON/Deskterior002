4a3bedaec7bdfd2d9611c5aefe971a1c8df1db18
hoon <danielhoon@naver.com>
2025-01-17 15:46:53 +0900

    데스크테리어 새로운버전 - 스플래시이미지 해보려는데 아직 잘 안됌

---
+++ app/src/main/java/com/example/desk0018/FirstSplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.desk0018;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class FirstSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_first_splash);
+
+        // Window Insets 설정
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // 4초 뒤에 MainActivity로 이동
+        new android.os.Handler().postDelayed(() -> {
+            // 다음 액티비티로 이동
+            Intent intent = new Intent(FirstSplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // 현재 액티비티 종료
+        }, 4000); // 4초 (4000ms)
+    }
+}
